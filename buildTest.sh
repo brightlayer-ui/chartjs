cff4759eb6a3f122c5b5bd673b1eef4e64c1b9ff
emclaug2 <evan.w.mclaughlin@gmail.com>
2019-09-17 10:45:08 -0400

    Updated buildtest to check for required files

--- buildTest.sh
+++ buildTest.sh
@@ -12,24 +12,9 @@ echo -ne "  readme: "
 if [ ! -f ./README.md ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
 echo -ne "  license: "
 if [ ! -f ./LICENSE ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
-
-echo "Checking Demo (Angular)"
-echo "Checking for required files..."
-echo -ne "  package.json: "
-if [ ! -f demo-angular/package.json ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
-echo -ne "  readme: "
-if [ ! -f demo-angular/Readme.md ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
-
-
-echo "Checking Demo (React))"
-echo "Checking for required files..."
-echo -ne "  package.json: "
-if [ ! -f demo-react/package.json ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
-echo -ne "  readme: "
-if [ ! -f demo-react/Readme.md ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
-
-echo -e "\r\n${GREEN}-----------------------------------"
-echo -e "All ChartJS files are found."
-echo -e "-----------------------------------${NC}\r\n\r\n"
+echo -ne "  main: "
+if [ ! -f ./dist/index.js ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
+echo -ne "  type definitions: "
+if [ ! -f ./dist/index.d.ts ]; then echo -e "${RED}Not Found${NC}" && exit 1; else echo -e "${GREEN}Found${NC}"; fi
 
 exit 0
