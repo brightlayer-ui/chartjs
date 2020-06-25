d4e9762bd7df8c9253b3ff318a929f7db1a543f3
daileytj <daileytj89@gmail.com>
2020-06-25 14:01:11 -0400

    Add copyright information

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+/**
+Copyright (c) 2018-present, Eaton
+ 
+All rights reserved.
+ 
+This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
+**/
+
 import {ChartConfiguration} from "chart.js";
 import {pxbBarChartData, pxbBarChartLabels, pxbBarChartOptions} from './config/pxb-barchart-config';
 import {pxbDonutChartData, pxbDonutChartLabels, pxbDonutChartOptions} from './config/pxb-donut-config';
