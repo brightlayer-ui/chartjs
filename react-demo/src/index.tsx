85c1352eb1d2eaab0ed9a9c065b1deb172d7bb54
Greiner <E0458886@eaton.ad.etn.com>
2020-10-27 14:30:14 -0400

    update import

--- react-demo/src/index.tsx
+++ react-demo/src/index.tsx
@@ -10,7 +10,7 @@ import {
    createMuiTheme
 } from '@material-ui/core/styles';
 // @ts-ignore
-import * as PXBlueThemes from '@pxblue/themes/react';
+import * as PXBlueThemes from '@pxblue/react-themes';
 
 require('typeface-open-sans');
 
