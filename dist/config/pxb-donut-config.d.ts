dfb535fcde7e3a113cc7d3770ceea0dec4376475
Firm-Soft <evan.w.mclaughlin@gmail.com>
2021-02-02 14:07:02 -0500

    Remove dist folder + add publishing

--- dist/config/pxb-donut-config.d.ts
+++
@@ -1,4 +0,0 @@
-import { ChartDataSets, ChartOptions } from "chart.js";
-export declare const pxbDonutChartOptions: ChartOptions;
-export declare const pxbDonutChartLabels: string[];
-export declare const pxbDonutChartData: ChartDataSets[];
--- dist/util/styles.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const OpenSans = "Open Sans";
-export declare const pxbColorWheel: any[];
-export declare const pxbColors: any;
